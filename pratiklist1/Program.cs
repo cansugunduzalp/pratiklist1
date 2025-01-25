2e777de327b40483fc6c730a6f162dae97bd8322
cansugunduzalp <cansugunduzalp@gmail.com>
2025-01-26 00:23:34 +0300

    Add project files.

---
+++ pratiklist1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Yönetim isimlerini içeren bir liste oluşturuyoruz
+        List<string> davetliler = new List<string>
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        // foreach döngüsü ile isimleri ekrana yazdırıyoruz
+        foreach (string davetli in davetliler)
+        {
+            Console.WriteLine(davetli);
+        }
+    }
+}
